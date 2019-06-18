5b68d69268e2742e3a2939e5fa19bfc7d1c13039
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-06-18 18:15:09 -0400

    [Neeraj] Created respected File

--- cityTransitSystemSwift/Rider.swift
+++ cityTransitSystemSwift/Rider.swift
@@ -1,11 +1,9 @@
 //
-//  main.swift
+//  Rider.swift
 //  cityTransitSystemSwift
 //
-//  Created by Neeraj Prasher on 2019-06-17.
+//  Created by Neeraj Prasher on 2019-06-18.
 //  Copyright © 2019 lambton. All rights reserved.
 //
 
 import Foundation
-
-
